29ed4f8ddebb7ec674c28c04221344eacabf0373
kudesnick <kudesnick@inbox.ru>
2017-06-02 13:38:22 +0300

    Реализовал (не тестировал в железе) Модуль буфера очереди FIFO

---
+++ f105_CMSIS/src/main/fifo.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// Буфер данных
+//------------------------------------------------------------------------------
+
+#ifndef _FIFO_H_
+#define _FIFO_H_
+
+#define FIFO_T(type,size) struct{uint16_t begin_idx, end_idx; type data[size];}
+
+#define FIFO_GET_SIZE(queue)        (sizeof(queue.data)/sizeof(queue.data[0]))
+#define FIFO_GET_FULL(queue)        ((queue.end_idx - queue.begin_idx) & (FIFO_GET_SIZE(queue) - 1))
+#define FIFO_ADD(queue,item)        (queue.data[queue.end_idx++ & (FIFO_GET_SIZE(queue) - 1)] = item)
+#define FIFO_EXTRACT(queue)         (queue.data[queue.begin_idx++ & (FIFO_GET_SIZE(queue) - 1)])
+#define FIFO_READ_HEAD(queue)       (queue.data[queue.begin_idx & (FIFO_GET_SIZE(queue) - 1)])
+#define FIFO_READ_ITEM(queue,item)  (queue.data[((queue.begin_idx) + item) & (FIFO_GET_SIZE(queue) - 1)])
+#define FIFO_CLEAR(queue)           (queue.end_idx = queue.begin_idx)
+
+#endif /* _FIFO_H_ */
