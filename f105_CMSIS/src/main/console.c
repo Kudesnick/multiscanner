bf5038f53b03b7d0b249b26be1f080fd5fdb2a8a
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2017-06-17 21:50:07 +0300

    Добавил алгоритмы  расшифровки команд,принятых с консоли.

--- f105_CMSIS/src/main/console.c
+++ f105_CMSIS/src/main/console.c
@@ -5,26 +5,32 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "fifo.h"
 #include "bsp_con.h"
+
+#define is_delim(s) ((bool)(s == ' ' || s == 0x0A || s == 0x0D))
+
+typedef void(cmd_func_t)(uint8_t *buf, uint8_t size);
 
 typedef const struct
 {
-    const uint8_t *cmd_name;
-    const void(*cmd_func)(uint8_t *buf, uint8_t size);
+    const uint8_t *name;
+    const uint_fast8_t size; // Длину строк приходится проставлять вручную. Пока не придумал, что с этим делать
+    cmd_func_t *const func;
 } console_cmd_t;
 
 // константы команд консоли
 console_cmd_t console_cmd_list[] =
 {
-    {(uint8_t *)"help", NULL}, // Получить справку о программе
-    {(uint8_t *)"get" , NULL}, // Получить параметры настройки интерфейса
-    {(uint8_t *)"set" , NULL}, // Настроить интерфейс
-    {(uint8_t *)"can1", NULL}, // Отправить сообщение по can1
-    {(uint8_t *)"can2", NULL}, // Отправить сообщение по can2
-    {(uint8_t *)"lin1", NULL}, // Отправить сообщение по lin1
-    {(uint8_t *)"lin2", NULL}, // Отправить сообщение по lin2
-    {(uint8_t *)"urt1", NULL}, // Отправить сообщение по uart1
-    {(uint8_t *)"urt2", NULL}, // Отправить сообщение по uart2 
+    {(const uint8_t *)"help", 4, NULL}, // Получить справку о программе
+    {(const uint8_t *)"get" , 3, NULL}, // Получить параметры настройки интерфейса
+    {(const uint8_t *)"set" , 3, NULL}, // Настроить интерфейс
+    {(const uint8_t *)"can1", 4, NULL}, // Отправить сообщение по can1
+    {(const uint8_t *)"can2", 4, NULL}, // Отправить сообщение по can2
+    {(const uint8_t *)"lin1", 4, NULL}, // Отправить сообщение по lin1
+    {(const uint8_t *)"lin2", 4, NULL}, // Отправить сообщение по lin2
+    {(const uint8_t *)"urt1", 4, NULL}, // Отправить сообщение по uart1
+    {(const uint8_t *)"urt2", 4, NULL}, // Отправить сообщение по uart2 
 };
 
 // Приведение к нижнему регистру
@@ -47,7 +53,7 @@ static uint8_t console_get_param_count(uint8_t *buf, uint8_t size)
     
     for (uint_fast16_t i = 0; i < size; i++)
     {
-        if (buf[i] == ' ' || buf[i] == 0x0A || buf[i] == 0x0D)
+        if (is_delim(buf[i]))
         {
             start_count = false;
         }
@@ -62,15 +68,17 @@ static uint8_t console_get_param_count(uint8_t *buf, uint8_t size)
 }
 
 // Возвращает размер и указатель на лексему под номером num
-static uint8_t console_get_param(uint8_t num, uint8_t *buf, uint8_t *param_ptr)
+static uint8_t console_get_param(uint8_t num, uint8_t *buf, uint8_t size, uint8_t **param_ptr)
 {
     uint8_t count = 0; // текущий номер лексемы
-    uint8_t size = 0; // Размер лексемы
+    uint8_t param_size = 0; // Размер лексемы
     bool start_count = false; // Флаг того, что символ лексемы - не первый
+    
+    num++;
     
     for (uint_fast16_t i = 0; i < size; i++)
     {
-        if (buf[i] == ' ' || buf[i] == 0x0A || buf[i] == 0x0D)
+        if (is_delim(buf[i]))
         {
             start_count = false;
             if (count == num)
@@ -86,22 +94,22 @@ static uint8_t console_get_param(uint8_t num, uint8_t *buf, uint8_t *param_ptr)
                 start_count = true;
                 if (count == num)
                 {
-                    param_ptr = &buf[i];
+                    *param_ptr = &buf[i];
                 }
             }
             if (count == num)
             {
-                size++;
+                param_size++;
             }
         }
     }
     
-    if (size == 0)
+    if (param_size == 0)
     {
-        param_ptr = NULL;
+        *param_ptr = NULL;
     }
     
-    return size;
+    return param_size;
 }
 
 // Парсинг команд консоли
@@ -111,15 +119,15 @@ static void console_cmd_parser(uint8_t *buf, uint8_t size)
     if (console_get_param_count(buf, size) > 0)
     {
         uint8_t *cmd_ptr;
-        uint8_t cmd_size = console_get_param(0, buf, cmd_ptr);
-        for (uint8_t i = 0; i < sizeof(console_cmd_list)/sizeof(console_cmd_list[0]); i++)
+        uint8_t cmd_size = console_get_param(0, buf, size, &cmd_ptr);
+        for (uint8_t i = 0; i < sizeof_arr(console_cmd_list); i++)
         {
-            if (sizeof(*console_cmd_list[i].cmd_name) == cmd_size
-                && memcmp(buf, console_cmd_list[i].cmd_name, cmd_size) == 0
-                && console_cmd_list[i].cmd_func != NULL
+            if (console_cmd_list[i].size == cmd_size
+                && memcmp(buf, console_cmd_list[i].name, cmd_size) == 0
+                && console_cmd_list[i].func != NULL
                 )
             {
-                console_cmd_list[i].cmd_func(buf, size);
+                console_cmd_list[i].func(buf, size);
             }
         }
     }
