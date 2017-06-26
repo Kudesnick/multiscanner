107841b19525ee91d89485430f4fbd70db1c31dc
Stulov Tikhon Sergeevich <stulov@author.spb.ru>
2017-06-26 18:29:36 +0300

    Исправил механизм определения заполнения буфера FIFO

--- f105_CMSIS/src/main/fifo.h
+++ f105_CMSIS/src/main/fifo.h
@@ -10,7 +10,7 @@
 #define FIFO_T(type,size) struct{uint16_t begin_idx, end_idx; type data[size];}
 
 #define FIFO_GET_SIZE(queue)        (sizeof_arr(queue.data))
-#define FIFO_GET_FULL(queue)        ((queue.end_idx - queue.begin_idx) & (FIFO_GET_SIZE(queue) - 1))
+#define FIFO_GET_FULL(queue)        (queue.end_idx - queue.begin_idx)
 #define FIFO_ADD(queue,item)        (queue.data[queue.end_idx++ & (FIFO_GET_SIZE(queue) - 1)] = item)
 #define FIFO_EXTRACT(queue)         (queue.data[queue.begin_idx++ & (FIFO_GET_SIZE(queue) - 1)])
 #define FIFO_READ_HEAD(queue)       (queue.data[queue.begin_idx & (FIFO_GET_SIZE(queue) - 1)])
