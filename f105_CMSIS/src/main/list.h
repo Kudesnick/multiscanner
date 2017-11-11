6c485f7f556de025b8b649fb0dadf80f8be4811b
kudesnick <kudesnick@inbox.ru>
2017-11-11 22:50:28 +0300

    Задокументировал модуль list.h

--- f105_CMSIS/src/main/list.h
+++ f105_CMSIS/src/main/list.h
@@ -2,8 +2,7 @@
  *  @file list.h
  *
  *  @brief шаблон класса списка
- *
- *  используется для автоматической генерации списков объектов
+ *  @details используется для автоматической генерации списков объектов
  */
 
 #ifndef _LIST_H_
@@ -14,54 +13,65 @@
 
 /**
  *  @brief Типы списков для разграничения списков объектов
- *  
- *  В зависимости от того, какой list_type_t указывается при наследовании шаблона,
+ *  @details В зависимости от того, какой list_type_t указывается при наследовании шаблона,
  *  объект попадает в тот или иной список. Возможно множественное наследование,
  *  тогда объект попадает в несколько списков.
  */
 typedef enum
 {
-    LIST_TYPE_DEFAULT,  /**< Тип по умолчанию */
-    LIST_TYPE_UNIT,     /**< Тип ввода/вывода. (Используется модулем абстрактного класса ввода-вывода) */
-    LIST_TYPE_THREAD,   /**< Тип потока. (Используется модулем класса-потока) */
-    LIST_TYPE_PARSER    /**< Тип парсера. (Используется для объектов парсера консольных команд) */
+    LIST_TYPE_DEFAULT,  ///< Тип по умолчанию
+    LIST_TYPE_UNIT,     ///< Тип ввода/вывода. (Используется модулем абстрактного класса ввода-вывода)
+    LIST_TYPE_THREAD,   ///< Тип потока. (Используется модулем класса-потока)
+    LIST_TYPE_PARSER    ///< Тип парсера. (Используется для объектов парсера консольных команд)
 }list_type_t;
 
 /**
  *  @brief Шаблон класса списка
- *
- *  Объекты, наследуемые от этого шаблона автоматически при создании попадают в тот или иной список,
+ *  @details Объекты, наследуемые от этого шаблона автоматически при создании попадают в тот или иной список,
  *  в зависимости от значения list_type. Допускается множественное наследование для попадания
  *  объекта сразу в несколько списков
  *  
  *  @param[in] list_type Тип списка
  *  
- *  @warning Деструктор не тестировался.
- *  
- *  @todo Переделать, где надо, uint16_t на list_type_t, написать деструктор
- *
+ *  @warning Деструктора нет. Использовать только со статически создаваемыми объектами
  */
 template <const uint16_t list_type> class cpp_list
 {
     private:
+#warning поменять uint16_t на list_type_t
         static const uint16_t obj_list_type = list_type;
-        static cpp_list *last_pointer; /**< Указатель на последний элемент в списке */
-        cpp_list *prev_pointer;        /**< Предыдущий элемент списка */
-        uint16_t class_type;           /**< Тип модуля (класса) */
-        uint16_t object_name;          /**< Имя объекта */
+        static cpp_list *last_pointer; ///< Указатель на последний элемент в списке
+        cpp_list *prev_pointer;        ///< Предыдущий элемент списка
+        uint16_t class_type;           ///< Тип модуля (класса)
+        uint16_t object_name;          ///< Имя объекта
     protected:
     public:
         cpp_list(uint16_t _class_type = NULL, uint16_t _object_name = NULL);
-        cpp_list* get_prev_pointer(void);            // Получить указатель на предыдущий элемент списка
-        static cpp_list* get_last_pointer(void);     // Получить указатель на последний элемент в списке
-        uint16_t get_list_type(void);                // Возвращает тип списка
-        uint16_t get_class_type(void);               // Возвращает тип класса (задается в конструкторе производного класса)
-        uint16_t get_object_name(void);              // Возвращает имя объекта (задается при создании конкретного объекта, если надо)
-        static cpp_list* get_object(uint16_t _class_type, uint16_t _object_name); // Поиск объекта по имени
+        cpp_list* get_prev_pointer(void);            ///< Получить указатель на предыдущий элемент списка
+        static cpp_list* get_last_pointer(void);     ///< Получить указатель на последний элемент в списке
+        uint16_t get_list_type(void);                ///< Возвращает тип списка
+        uint16_t get_class_type(void);               ///< Возвращает тип класса (задается в конструкторе производного класса)
+        uint16_t get_object_name(void);              ///< Возвращает имя объекта (задается при создании конкретного объекта, если надо)
+        static cpp_list* get_object(uint16_t _class_type, uint16_t _object_name); ///< Поиск объекта по имени
 };
 
+/**
+ *  @brief Указатель на последний элемент в списке
+ *  @details элемент указывает на последний созданный объект класса-наследника списка
+ */
 template <const uint16_t list_type> cpp_list<list_type> * cpp_list<list_type>::last_pointer = NULL;
 
+/**
+ *  @brief Конструктор
+ *  @details Помимо типа списка, жестко задаваемого типизацией шаблона, мы можем разделить элементы в списке
+ *           по их типу и имени. И то и другое - перечисляемые параметры. Они не влияют на функционал объекта,
+ *           но могут быть использованы для построения сложной иерархии объектов
+ *
+ *  @param[in] _class_type тип объекта списка
+ *  @param[in] _object_name имя объекта
+ *
+ *  @return указатель на объект
+ */
 template <const uint16_t list_type> cpp_list<list_type>::cpp_list(uint16_t _class_type, uint16_t _object_name):
     class_type(_class_type),
     object_name(_object_name)
@@ -70,39 +80,90 @@ template <const uint16_t list_type> cpp_list<list_type>::cpp_list(uint16_t _clas
     last_pointer = this;
 };
 
+/**
+ *  @brief Получить указатель на предыдущий элемент списка
+ *
+ *  @return указатель на объект. NULL, если текущий объект - первый в списке
+ */
 template <const uint16_t list_type> cpp_list<list_type> * cpp_list<list_type>::get_prev_pointer(void)
 {
     return prev_pointer;
 };
 
+/**
+ *  @brief Получить указатель на последний элемент в списке
+ *
+ *  @return указатель на объект. NULL, если список пуст
+ */
 template <const uint16_t list_type> cpp_list<list_type> * cpp_list<list_type>::get_last_pointer(void)
 {
     return last_pointer;
 };
 
+/**
+ *  @brief Возвращает тип списка
+ *  @details используется для определения типа списка к которому пренадлежит какой-либо конкретный объект.
+ *           Единственное ограничение - объект должен наследоваться от данного шаблона.
+ *           Предназначен для детектирования списка, если тип объекта неизвестен.
+ *
+ *  @return Тип списка
+ */
 template <const uint16_t list_type> uint16_t cpp_list<list_type>::get_list_type(void)
 {
     return obj_list_type;
 };
 
+/**
+ *  @brief Возвращает тип класса (задается в конструкторе производного класса)
+ *  @details используется для определения типа класса к которому пренадлежит какой-либо конкретный объект.
+ *           Единственное ограничение - объект должен наследоваться от данного шаблона.
+ *
+ *  @return Тип класса
+ */
 template <const uint16_t list_type> uint16_t cpp_list<list_type>::get_class_type(void)
 {
     return class_type;
 };
 
+/**
+ *  @brief Возвращает имя объекта (задается при создании конкретного объекта, если надо)
+ *  @details используется для определения имени конкретного объекта.
+ *           Единственное ограничение - объект должен наследоваться от данного шаблона.
+ *
+ *  @warning Следует учесть, что имена задаются в консрукторе объекта произвольно и никак не привязаны
+ *           к его свойствам. Также не гарантируется уникальность имен.
+ *
+ *  @return Имя объекта
+ */
 template <const uint16_t list_type> uint16_t cpp_list<list_type>::get_object_name(void)
 {
     return object_name;
 };
 
+/**
+ *  @brief Поиск объекта по имени
+ *  @details находит последний добавленный в список объект с совпадающим именем и типом объекта и возвращает указатель на него.
+ *           если _class_type указан как NULL, то ищет только по имени (игнорируя тип). Если объект не найден - возвращает NULL.
+ *
+ *  @warning Следует учесть, что имена задаются в консрукторе объекта произвольно и никак не привязаны
+ *           к его свойствам. Также не гарантируется уникальность имен.
+ *
+ *  @param[in] _class_type Тип класса искомого объекта. Если NULL - любой тип
+ *  @param[in] _object_name имя искомого объекта.
+ *
+ *  @return Указатель на объект. Ели не найден - возвращает NULL
+ */
 template <const uint16_t list_type> cpp_list<list_type> * cpp_list<list_type>::get_object(uint16_t _class_type, uint16_t _object_name)
 {
     cpp_list * ptr = get_last_pointer();
-    
-    while (ptr == NULL)
+
+#warning изменил алгоритм, но сейчас проект не компиллится - надо тестить в последствии   
+    while (ptr != NULL)
     {
-        if ((ptr->get_class_type() == _class_type)
-            && (ptr->get_object_name() == _object_name)
+        if (   ptr->get_object_name() == _object_name
+            && (   _class_type == NULL
+                || ptr->get_class_type() == _class_type
+                )
             )
         {
             break;
