3cb1007e70b782054ac60ae04ba8df67e6f42309
kudesnick <kudesnick@inbox.ru>
2017-11-12 22:56:19 +0300

    Корректировка структуры буферов.

--- f105_CMSIS/src/main/fifo_buffers.h
+++ f105_CMSIS/src/main/fifo_buffers.h
@@ -5,22 +5,18 @@
 #ifndef _FIFO_BUFFERS_H_
 #define _FIFO_BUFFERS_H_
 
-#include "stdint.h"
+#include <stdint.h>
 
 #include "fifo.h"
-#include "iface.h"
+#include "msg_types.h"
+#include "units_config.h"
 
-// Промежуточный буфер для последовательного порта
-
-#define RX_BUFFER_SIZE FIFO_SIZE_16
-#define TX_BUFFER_SIZE FIFO_SIZE_16
-
-class fifo_serial
+class fifo_buff
 {
 	public:
-		cpp_fifo<msg_serial_t, RX_BUFFER_SIZE> rx;
-		cpp_fifo<msg_serial_t, TX_BUFFER_SIZE> tx;
-		fifo_serial(void);
+		cpp_fifo<msg_t, IFACES_TX_BUFFER_SIZE> tx;
+		cpp_fifo<msg_t, IFACES_RX_BUFFER_SIZE> rx;
+		fifo_buff(void);
 };
 
-#endif /* _FIFO_BUFFERS_H_ */
+#endif /* _FIFO_BUFF_H_ */
