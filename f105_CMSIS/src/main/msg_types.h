918ac51384b3fef9d6f57e06b6c46a277e05a686
kudesnick <kudesnick@inbox.ru>
2017-11-14 22:19:34 +0300

    окончательно определился со структурой сообщений.

--- f105_CMSIS/src/main/msg_types.h
+++ f105_CMSIS/src/main/msg_types.h
@@ -14,7 +14,7 @@
 
 #define CAN_DATA_LEN_MAX 8
 #define LIN_DATA_LEN_MAX 8
-#define UART_DATA_LEN_MAX (sizeof(msg_can_t) - 1) // Высчитывается, чтобы занимаемая память не превышала длину can-сообщения
+#define UART_DATA_LEN_MAX 16
 
 #pragma pack(1) // пакуем все структуры в этом модуле. Они используются в буферах.
 
@@ -25,14 +25,21 @@ typedef enum : uint8_t
     MSG_RX
 } msg_direction_t;
 
-/// Заголовок сообщения, универсальный для всех интерфейсов
+/// Заголовок сообщения, подготовленного для передачи, универсальный для всех интерфейсов
 struct msg_header_t
 {
     iface_type_t msg_type;  ///< Тип сообщения
     iface_name_t route;     ///< В какой интерфейс передаем
     msg_direction_t direct; ///< Направление передачи
-    uint64_t time;          ///< метка времени
-    uint64_t interval;      ///< Интервал для повторений
+    union
+    {
+        struct
+        {
+            uint32_t counter;      ///< Отсчет времеи до следующей передачи
+            uint32_t interval;      ///< Интервал для повторений
+        } tx_timer;
+        uint64_t rx_timestamp;
+    }
     uint16_t count;         ///< Счетчик повторений
 };
 
@@ -55,13 +62,13 @@ typedef enum : uint8_t
 
 typedef uint32_t can_id_t;
 
-typedef struct
+typedef struct : msg_header_t
 {
-    msg_brk_reason_can_t reason : 4; ///< Причина окончания сообщения (или ошибка отправки)
-    uint8_t len                 : 4;
-    can_id_t id   : 29;
-    can_id_t type :  2;
+    can_id_t id;
+    can_msg_type_t type;
+    uint8_t len;
     uint8_t data[CAN_DATA_LEN_MAX];
+    msg_brk_reason_can_t reason; ///< Причина окончания сообщения (или ошибка отправки)
 } msg_can_t;
 
 // Структуры тела сообщения LIN
@@ -81,14 +88,14 @@ typedef enum : uint8_t
     LIN_TYPE_V2,
 } lin_msg_type_t;
 
-typedef struct
+typedef struct : msg_header_t
 {
-    msg_brk_reason_lin_t reason; ///< Причина окончания сообщения (или ошибка отправки)
-    uint8_t len;
-    can_msg_type_t type;
     uint8_t id;
+    can_msg_type_t type;
+    uint8_t len;
     uint8_t data[LIN_DATA_LEN_MAX];
     uint8_t crc;
+    msg_brk_reason_lin_t reason; ///< Причина окончания сообщения (или ошибка отправки)
 } msg_lin_t;
 
 // Структуры тела сообщения UART
@@ -106,24 +113,21 @@ typedef enum : uint8_t
     MSG_BRK_UART_PARITY_ERR,  ///< Ошибка паритета
 } msg_brk_reason_uart_t;
 
-typedef struct
+typedef struct : msg_header_t
 {
-    msg_brk_reason_lin_t reason : 4; ///< Причина окончания сообщения (или ошибка отправки)
-    uint8_t len                 : 4;
+    uint8_t len;
     uint8_t data[UART_DATA_LEN_MAX];
+    msg_brk_reason_lin_t reason; ///< Причина окончания сообщения (или ошибка отправки)
 } msg_uart_t;
 
 /// Структура сообщения универсальная
 //-----------------------------------
-typedef struct : msg_header_t
+typedef union
 {
-    union
-    {
 #warning проверить, что в итоге все варианты занимают одинаковый объем памяти
-        msg_can_t  can;
-        msg_lin_t  lin;
-        msg_uart_t uart;
-    } msg;
+    msg_can_t  can;
+    msg_lin_t  lin;
+    msg_uart_t uart;
 } msg_t;
 
 #endif /* _MSG_TYPES_ */
