2e4ae7d83ea18af6071e4cd85f0731be07d8376b
kudesnick <kudesnick@inbox.ru>
2018-01-30 05:47:05 +0300

    Добавил подсистему SEGGER_RTT для отладки через окно терминала.

--- f105_CMSIS/src/main/main.cpp
+++ f105_CMSIS/src/main/main.cpp
@@ -8,9 +8,17 @@
  *  @date    
  */
 
+#include <stdio.h>
+#include "SEGGER_RTT.h"
+
 #include "multiscanner.h"
 
 int main(void)
-{   
+{
+#ifdef DEBUG
+    SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+    SEGGER_RTT_WriteString(0, "SEGGER RTT Debugging is run.\r\n");
+#endif
+    
     return cpp_multiscanner::routine();
 };
